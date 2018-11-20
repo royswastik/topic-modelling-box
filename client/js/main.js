6e934c63d7de912a93609eb93cc8a87df887f0d6
Swastik Roy <swastik.roy@live.com>
2018-11-20 02:55:44 -0700

    Fix JSON names

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,7 @@ function loadD3(){
     d3.csv("data/document_topic_probability.json", function(x){
         window.document_topic_probability= x;
         d3.csv("data/topic_word_distribution_in_corpora.json", function(y){
-            window.topic_word_distribution_in_corpora= y;
+            window.word_distribution_in_corpora= y;
             d3.csv("data/topic_word_probability_in_topic.json", function(z){
                 window.topic_word_probability_in_topic = z;
                 generateTopicVectors(); //Loads vectors in window.topicVectors
