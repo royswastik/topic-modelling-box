4f9530445c2b234c4a528a1eabe73b345bf8fad0
Swastik Roy <swastik.roy@live.com>
2018-11-20 01:52:17 -0700

    Add data load in main.s

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,14 @@
 function loadD3(){
     console.log("hello");
-    d3.csv("data.json", function(x){
-        window.data1= x;
-        loadVisualizations();
+    d3.csv("data/document_topic_probability.json", function(x){
+        window.document_topic_probability= x;
+        d3.csv("data/document_topic_probability.json", function(y){
+            window.topic_word_distribution_in_corpora= y;
+            d3.csv("data/document_topic_probability.json", function(z){
+                window.topic_word_probability_in_topic = z;
+                loadVisualizations();
+            });
+        });
     });
 }
 
